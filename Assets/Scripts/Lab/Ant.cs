2cc319b7c7c6eb78744fdfc65cecf595b1f50f6d
Patipong123 <patipong.saet@bumail.net>
2024-09-24 11:13:38 +0700

    Add Ant and Crocodile Scripts

---
+++ Assets/Scripts/Lab/Ant.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ant : Enemy
+{
+    private Vector2 velocity;
+    private Transform[] movePoint;
+
+    public override void Behaviour()
+    {
+        Debug.Log("Anttttttttttttttttttt.Behaviour");
+    }
+}
---
+++ Assets/Scripts/Lab/Crocodile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crocodile : Enemy
+{
+    private float attackRange;
+    private Player player;
+
+    public override void Behaviour()
+    {
+        Debug.Log("Crocodileeeeeeeeeeeeee.Behaviour");
+    }
+}
